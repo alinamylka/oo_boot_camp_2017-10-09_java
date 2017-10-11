f6f894acde87d19fe1fdefd5e6b9d84b8fd07309
u216116 <alina_maria.mylka@sbb.ch>
2017-10-11 11:15:13 +0200

    addition check for temperature

---
+++ src/quantity/RatioQuantity.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2017 by Fred George
+ * May be used freely except for training; license required for training.
+ */
+
+package quantity;
+
+
+// Understands a specific ratio measurement
+public class RatioQuantity extends Quantity<RatioUnit> {
+
+    RatioQuantity(double amount, RatioUnit unit) {
+        super(amount, unit);
+    }
+
+    public RatioQuantity plus(RatioQuantity other) {
+        return new RatioQuantity(this.amount + this.convertedAmount(other), this.unit);
+    }
+
+    public RatioQuantity minus(RatioQuantity other) {
+        return this.plus(other.negate());
+    }
+
+    public RatioQuantity negate() {
+        return new RatioQuantity(-amount, unit);
+    }
+
+    @Override
+    public String toString() {
+        return "Quantity{" +
+                "amount=" + amount +
+                ", unit=" + unit +
+                '}';
+    }
+}
---
+++ src/quantity/RatioUnit.java
@@ -0,0 +1,30 @@
+package quantity;
+
+// Understands a ratio metric
+public class RatioUnit extends Unit {
+
+    public static final RatioUnit TEASPOON = new RatioUnit();
+    public static final RatioUnit TABLESPOON = new RatioUnit(3, TEASPOON);
+    public static final RatioUnit OUNCE = new RatioUnit(2, TABLESPOON);
+    public static final RatioUnit CUP = new RatioUnit(8, OUNCE);
+    public static final RatioUnit PINT = new RatioUnit(2, CUP);
+    public static final RatioUnit QUART = new RatioUnit(2, PINT);
+    public static final RatioUnit GALLON = new RatioUnit(4, QUART);
+    public static final RatioUnit INCH = new RatioUnit();
+    public static final RatioUnit FOOT = new RatioUnit(12, INCH);
+    public static final RatioUnit YARD = new RatioUnit(3, FOOT);
+    public static final RatioUnit FURLONG = new RatioUnit(220, YARD);
+    public static final RatioUnit MILE = new RatioUnit(8, FURLONG);
+
+    public RatioUnit() {
+        super();
+    }
+
+    public RatioUnit(int amount, RatioUnit ratioUnit) {
+        super(amount, ratioUnit);
+    }
+
+    public RatioQuantity s(double amount) {
+        return new RatioQuantity(amount, this);
+    }
+}

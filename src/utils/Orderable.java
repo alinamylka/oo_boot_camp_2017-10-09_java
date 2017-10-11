25de5dad87940b9155eef4826dd7aa1a1b9bd89c
u216116 <alina_maria.mylka@sbb.ch>
2017-10-11 14:20:18 +0200

    add sort for Rectangle.java

--- src/utils/Orderable.java
+++ src/utils/Orderable.java
@@ -4,11 +4,12 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+// Understands the order of the objects
 public interface Orderable<T extends Orderable> {
     int isBetterThen(T other);
 
-    static <T extends Orderable> List<T> sort(T... elemnts) {
-        return sort(Arrays.asList(elemnts));
+    static <T extends Orderable> List<T> sort(T... elements) {
+        return sort(Arrays.asList(elements));
     }
 
     static <T extends Orderable> List<T> sort(List<T> elements) {

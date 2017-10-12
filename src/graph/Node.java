245f25e7d84a72be080f7e7e0b8f87b409bb6752
u216116 <alina_maria.mylka@sbb.ch>
2017-10-12 10:17:09 +0200

    added java9

--- src/graph/Node.java
+++ src/graph/Node.java
@@ -13,6 +13,8 @@ import java.util.Set;
 
 public class Node {
 
+    public static final int ONE_HOPE = 1;
+    public static final int NO_HOPE = 0;
     private final List<Node> neightbors = new ArrayList<>();
 
     public Node to(Node neighbor) {
@@ -33,14 +35,14 @@ public class Node {
     }
 
     private Optional<Integer> hopCount(Node destination, Set<Node> visitedNodes) {
-        if (this == destination) return Optional.of(0);
+        if (this == destination) return Optional.of(NO_HOPE);
         if (visitedNodes.contains(this)) return Optional.empty();
 
         visitedNodes.add(this);
 
         return neightbors.stream()
                 .map(n -> n.hopCount(destination, visitedNodes)
-                        .map(node -> node + 1))
+                        .map(node -> node + ONE_HOPE))
                 .flatMap(Optional::stream)
                 .findAny();
     }
